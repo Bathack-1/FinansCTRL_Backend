05f4a85f5dfd695aa412d3a6b80a31cc628764a6
Chris <91430652+Bathack-1@users.noreply.github.com>
2025-09-17 15:41:24 +0200

    fjernet entry-pointet

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import datetime
 from transaksjoner import Transaksjoner, Database
 from personer import Personer
 from kategorier import Kategorier
-
-def dagen_idag():
-    return datetime.datetime.now().date().isoformat()
-
-
-db = Database("database.db")
-
-personer = Personer(db)
-kategorier = Kategorier(db)
-transaksjoner = Transaksjoner(db, personer, kategorier)
